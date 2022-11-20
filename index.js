6aa514472ff1225898df6e296aba1c151aa2dacd
6dean <ferdjallahrockdean@gmail.com>
2022-11-20 12:17:50 +0100

    all roads redirection

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(comicsRoute);
 app.use(charactersRoute);
 
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Ooops , are you lost ?" });
+});
+
 app.listen(3000, () => {
   console.log("Server is now online /!\\");
 });
