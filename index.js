4143a2b750f8ead9c0cf6d59db87653b98a24b41
6dean <ferdjallahrockdean@gmail.com>
2022-11-20 12:22:14 +0100

    port

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "Ooops , are you lost ?" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is now online /!\\");
 });
